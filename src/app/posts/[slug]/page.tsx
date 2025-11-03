7effe98114784cd733826c5cdc6dbb15200b952f
Xillioneur <241650870+Xillioneur@users.noreply.github.com>
2025-11-03 12:12:02 -0500

    Fix open graph images for blog posts on x.com

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -60,6 +60,13 @@ export async function generateMetadata(props: Params): Promise<Metadata> {
       title,
       images: [post.ogImage.url],
     },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description: 'A blog about God and Father God. Amen and Selah.',
+      creator: '@Xillioneur',
+      images: [post.ogImage.url], // Absolute URL
+    },
   };
 }
 
