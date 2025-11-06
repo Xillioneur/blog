ec313d0b26e630d74f42cdc3a1f844108611105e
Xillioneur <241650870+Xillioneur@users.noreply.github.com>
2025-11-05 21:10:54 -0500

    Fix broken links and more. Amen and selah.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const EXAMPLE_PATH = "blog-starter";
+export const LINK_TO_GITHUB_PROFILE_AND_POSTS = "https://github.com/Xillioneur/blog/tree/main";
 export const CMS_NAME = "Markdown";
 export const HOME_OG_IMAGE_URL =
   "/assets/opengraph-image.png";
